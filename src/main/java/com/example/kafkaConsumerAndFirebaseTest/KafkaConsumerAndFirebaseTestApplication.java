213550233a0d74c4ec583696237aab4a71595f9e
nooboolean <nooboolean@i.softbank.jp>
2020-10-11 21:44:44 +0900

    first commit

---
+++ src/main/java/com/example/kafkaConsumerAndFirebaseTest/KafkaConsumerAndFirebaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaConsumerAndFirebaseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerAndFirebaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerAndFirebaseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaConsumerAndFirebaseTest/KafkaConsumerAndFirebaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaConsumerAndFirebaseTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerAndFirebaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
